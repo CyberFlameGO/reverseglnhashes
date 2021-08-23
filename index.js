b56b939f230b8fcd04ce739f8ed9d2a03addd54c
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-08-23 20:02:09 +1200

    better format

--- index.js
+++ index.js
@@ -23,21 +23,22 @@ function reverseHashes(hashes) {
   const firstNums = "942930"
   let matches = 0
   save('{')
-  save(`"lastUpdated": ${Date.now()},`)
-  save('"glnHashGlnPairs":[')
+  save(`  "lastUpdatedAt": "${new Date().toISOString()}",`)
+  save('  "glnPairs": [')
   for(var i = 0; i <= 9999999; i++) {
     const gln = firstNums + (i + '').padStart(7, '0')
     const glnHash = base64.stringify(sha384(gln))
     if (hashes.includes(glnHash)) {
       matches++;
       if (matches >= hashes.length) {
-        save(`${JSON.stringify({gln, glnHash})}`)
+        save(`    ${JSON.stringify({gln, glnHash})}`)
         break
       }
-      save(`${JSON.stringify({gln, glnHash})},`)
+      save(`    ${JSON.stringify({gln, glnHash})},`)
     }
   }
-  save(']}')
+  save(']')
+  save('}')
 }
 
 main()
